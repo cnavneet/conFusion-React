7d9d5c59b3120f29e4bae7cda07d638241b2e76b
Navneet Choudhary <cnavneet58@gmail.com>
2023-12-18 14:28:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const store = ConfigureStore();
 class App extends Component {
 
   render() {
-    return (
+    return ( 
       <Provider store={store}>
         <BrowserRouter>
           <div>
